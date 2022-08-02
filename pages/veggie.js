d67a008a6aedc71be865d0995d231c5c007e9a48
Hannah Steil <h-steil@t-online.de>
2022-08-02 14:51:18 +0200

    Add Image from public directory

--- pages/veggie.js
+++ pages/veggie.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Veggie() {
   return (
@@ -8,6 +9,13 @@ export default function Veggie() {
       </Head>
       <h1>Veggie</h1>
       <p>Viele leckere Veggie Gerichte</p>
+      <Image
+        src="/../public/pizza.jpg"
+        alt="Pizza mit Gemüse"
+        layout="responsive"
+        width={2400}
+        height={1800}
+      />
     </>
   );
 }
